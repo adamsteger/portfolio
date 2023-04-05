dc84d140141d3372e4fdb1d4cc84bfed4eea2ea0
Adam Steger <asteger@email.sc.edu>
2023-04-05 14:21:19 -0400

    Created MyEpsilonRemover file

---
+++ Java/computation/MyEpsilonRemover.java
@@ -0,0 +1,13 @@
+package portfolio.Java.computation;
+
+public class MyEpsilonRemover {
+
+    private class NFA {
+        private class States {
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
